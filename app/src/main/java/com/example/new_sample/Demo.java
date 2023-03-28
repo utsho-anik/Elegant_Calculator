809980629a6eb11cda0eeecf186e0302d6e2fbc4
utsho-anik <anikdas548@gmail.com>
2023-03-28 13:41:35 +0600

    first change in master

---
+++ app/src/main/java/com/example/new_sample/Demo.java
@@ -0,0 +1,48 @@
+package com.example.new_sample;
+
+public class Demo {
+    int id;
+    int code;
+    String name,dept;
+    public Demo(){}
+
+    public Demo(int id,int code,String name,String  dept)
+    {
+        this.id=id;
+        this.code=code;
+        this.name=name;
+        this.dept=dept;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
